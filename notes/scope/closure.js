ddf341de10721641cef0af3268ade58d769a3794
Justin Thareja <justin.thareja@gmail.com>
2021-01-13 15:57:25 -0500

    updated closure notes

--- notes/scope/closure.js
+++ notes/scope/closure.js
@@ -1,8 +1,7 @@
-// Closure references the variable itself, not just a snapshot
-
-// Nice definition based on observable behavior
 // Closure is when a function "remembers" its lexical scope even when it's
-// executed outside of that lexical scope.
+// executed outside of the function's lexical scope.
+
+// Closure references the variable itself, not just a snapshot
 
 var teacher = "kyle"
 
@@ -12,3 +11,23 @@ function logTeacher() {
 
 teacher = "Justin"
 logTeacher(); // ??
+
+
+// CLOSURE - LOOPS
+
+// If you need multiple different values being closed over,
+// you need multiple different variables. 
+for (var i = 1; i <= 3; i++) {
+    setTimeout(function() {
+        console.log(`i: ${i}`);
+    }, i * 1000);
+}
+
+// In this case
+// since let is block scoped, each i is declared in its own "scope"
+// and can be treated as an individual variable
+for (let i = 1; i <= 3; i++) {
+    setTimeout(function () {
+        console.log(`i: ${i}`);
+    }, i * 1000);
+}
