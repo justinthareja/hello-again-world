bc0127446fff0c42cac55a921d29e2560128fcc8
Justin Thareja <justin.thareja@gmail.com>
2021-01-24 12:53:09 -0500

    annotated the web component boilerplate

--- hello-web-component/tooltip.js
+++ hello-web-component/tooltip.js
@@ -1,10 +1,13 @@
+// If required, define an HTML template using < template > and<slot>.
 const template = document.createElement("template");
 
 template.innerHTML = `
 
     <style>
 
-
+        .tooltip-container {
+            color: red;
+        }
 
     </style>
 
@@ -14,12 +17,24 @@ template.innerHTML = `
     </div>
 `;
 
+// Create a class in which you specify your web component functionality, using the 
+// ECMAScript 2015 class syntax (see Classes for more information). 
+
 class Tooltip extends HTMLElement {
     constructor() {
         super();
+
+        // If required, attach a shadow DOM to the custom element using Element.attachShadow() method.
+        // Add child elements, event listeners, etc., to the shadow DOM using regular DOM methods.
         this.attachShadow({ mode: "open" });
+
+        // Again use regular DOM methods to clone the template and attach it to your shadow DOM.
         this.shadowRoot.appendChild(template.content.cloneNode(true));
     }
 }
+
+// Register your new custom element using the CustomElementRegistry.define() method, 
+// passing it the element name to be defined, the class or function in which its functionality 
+// is specified, and optionally, what element it inherits from.
 
 window.customElements.define("tooltip-notify", Tooltip);
