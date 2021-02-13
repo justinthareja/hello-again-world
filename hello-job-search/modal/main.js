b7f492ec8f841c7395429c8b4fafbd39f605048f
Justin Thareja <justin.thareja@gmail.com>
2021-02-13 15:31:11 -0500

    updated modal to handle multiple offers

--- hello-job-search/modal/main.js
+++ hello-job-search/modal/main.js
@@ -1,22 +1,28 @@
-const $showOfferButton = document.querySelector(".js-show-offer");
+const $showOfferButtons = document.querySelectorAll(".js-show-offer");
 const $modal = document.querySelector(".js-modal");
 const $modalOverlay = document.querySelector(".js-modal-overlay");
 const $modalClose = document.querySelector(".js-close");
 const $modalAccept = document.querySelector(".js-accept");
 const $content = document.querySelector(".js-content");
 
-$showOfferButton.addEventListener("click", handleShowOfferButtonClick);
+let number = 0;
+
+$showOfferButtons.forEach($button =>
+    $button.addEventListener("click", handleShowOfferButtonClick)
+);
+
 $modalClose.addEventListener("click", handleModalCloseClick);
 $modalAccept.addEventListener("click", handleModalAcceptClick);
 $modalOverlay.addEventListener("click", handleModalOverlayClick);
 
 function handleShowOfferButtonClick(e) {
+    number = e.target.dataset.offer;
     openModal();
 }
 
 function handleModalAcceptClick(e) {
     $content.innerHTML = `
-        <div class="accept-message">Offer accepted!</div>
+        <div class="accept-message">Offer ${number} accepted!</div>
     `;
 
     closeModal();
