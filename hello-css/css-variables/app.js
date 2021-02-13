3b37026199ae74226aa54d6085ac604f0fbeaed2
Justin Thareja <justin.thareja@gmail.com>
2021-02-13 14:44:25 -0500

    updated css variables example with how to read css variables from root element

--- hello-css/css-variables/app.js
+++ hello-css/css-variables/app.js
@@ -3,6 +3,10 @@ const input = document.querySelector(".js-highlight");
 input.addEventListener("change", handleChange, false);
 input.addEventListener("input", handleChange, false);
 
+// Another way to set default value of color picker
+const rootStyles = window.getComputedStyle(document.documentElement);
+input.setAttribute("value", rootStyles.getPropertyValue("--highlight"));
+
 function handleChange(e) {
     const { name, value } = e.target;
     
