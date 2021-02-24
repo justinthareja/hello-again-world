6d562287c03e73b39cf4f127ad43f74eeb529a40
Justin Thareja <justin.thareja@gmail.com>
2021-02-24 14:55:49 -0500

    addedd notes when combining generators and promises

---
+++ hello-notes/kyle-simpson/generators/generators-plus-promises.js
@@ -0,0 +1,11 @@
+// this is the new baseline
+
+// yield out a promise then
+// resume a generator
+// in a circle
+
+// async function foo() {
+//     await ajax(...);
+// }
+
+// foo(); // returns a promise that gets resolved when the async function completes
